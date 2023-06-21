0eb43c811bc615d88c5272a67260febcfc9fa0cd
Lishan Abbas <lishan.dev@outlook.com>
2023-06-21 20:04:00 +0530

    Update navbar.tsx

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -5,6 +5,11 @@ import { GithubIcon, Mail } from "lucide-react";
 import Link from "next/link";
 
 export default function Navbar() {
+  function scrollToContact() {
+    const element = document.getElementById("contact")!;
+
+    element.scrollIntoView({ behavior: "smooth" });
+  }
   return (
     <nav className="flex justify-between items-center px-6 py-8 bg-transparent w-full top-0 max-w-4xl mx-auto">
       <Link
@@ -19,11 +24,7 @@ export default function Navbar() {
         lishan.dev
       </Link>
       <button
-        onClick={() =>
-          document
-            .getElementById("contact")
-            ?.scrollIntoView({ behavior: "smooth" })
-        }
+        onClick={scrollToContact}
         className="flex gap-2 items-center hover:underline hover:text-yellow-300 transition"
         aria-label="Go to Contact"
       >
