1a72655790400e0d13085fe88dc38132de9f202f
Lishan Abbas <lishan.dev@outlook.com>
2023-06-27 22:40:35 +0530

    SEO update

--- src/lib/seo.ts
+++ src/lib/seo.ts
@@ -1,101 +1,132 @@
-import { Metadata } from "next";
+import type {
+  Icons,
+  Robots,
+} from "next/dist/lib/metadata/types/metadata-types";
 
-const title = "lishan.dev | Building the Web, One Line at a Time";
-const description =
-  "Full-stack developer from the Maldives. 5+ years of experience crafting custom web solutions. Contact for inquiries.";
+import type { Metadata } from "next";
+import type { OpenGraph } from "next/dist/lib/metadata/types/opengraph-types";
+import type { Twitter } from "next/dist/lib/metadata/types/twitter-types";
+
+const domain = "lishan.dev";
 const url = new URL(
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
     : "https://lishan.dev"
 );
 
+const title = `${domain} | Building the Web, One Line at a Time`;
+const description =
+  "Full-stack developer from the Maldives. 5+ years of experience crafting custom web solutions. Contact for inquiries.";
+
+const keywords = [
+  domain,
+  "lishan",
+  "abbas",
+  "lishan abbas",
+  "dev",
+  "developer",
+  "web",
+  "full-stack",
+  "maldives",
+  "typescript",
+  "nextjs",
+  "react",
+];
+
+const robots: Robots = {
+  index: true,
+  follow: true,
+  "max-video-preview": -1,
+  "max-image-preview": "large",
+  "max-snippet": -1,
+};
+
+const icons: Icons = {
+  icon: [
+    {
+      url: "/assets/favicon-16x16.png",
+      type: "image/png",
+      sizes: "16x16",
+    },
+    {
+      url: "/assets/favicon-32x32.png",
+      type: "image/png",
+      sizes: "32x32",
+    },
+    {
+      url: "/assets/mstile-150x150.png",
+      type: "image/png",
+      sizes: "150x150",
+    },
+    {
+      url: "/assets/android-chrome-192x192.png",
+      type: "image/png",
+      sizes: "192x192",
+    },
+    {
+      url: "/assets/android-chrome-512x512.png",
+      type: "image/png",
+      sizes: "512x512",
+    },
+  ],
+  apple: {
+    url: "/apple-touch-icon.png",
+    sizes: "180x180",
+    type: "image/png",
+  },
+  other: [
+    {
+      url: "/assets/safari-pinned-tab.svg",
+      type: "image/svg+xml",
+      rel: "mask-icon",
+    },
+  ],
+};
+
+const openGraph: OpenGraph = {
+  title,
+  description,
+  url,
+  siteName: domain,
+  emails: ["lishan.dev@outlook.com"],
+  countryName: "Maldives",
+  images: [
+    {
+      url: "https://ik.imagekit.io/lishan/seo/og.jpg",
+      width: 1920,
+      height: 1080,
+    },
+  ],
+  locale: "en-US",
+  type: "website",
+};
+
+const twitter: Twitter = {
+  title,
+  site: "@lishan_dev",
+  card: "summary_large_image",
+};
+
 const defaultMetadata: Metadata = {
   metadataBase: url,
 
   title,
   description,
-  category: "Web Development",
-  keywords: [
-    "lishan",
-    "dev",
-    "developer",
-    "web",
-    "full-stack",
-    "maldives",
-    "typescript",
-    "nextjs",
-    "react",
-  ],
-  openGraph: {
-    title,
-    description,
-    url,
-    siteName: "lishan.dev",
-    emails: ["lishan.dev@outlook.com"],
-    countryName: "Maldives",
-    images: [
-      {
-        url: "https://ik.imagekit.io/lishan/seo/og.jpg",
-        width: 1920,
-        height: 1080,
-      },
-    ],
-    locale: "en-US",
-    type: "website",
-  },
-  twitter: {
-    title,
-    card: "summary_large_image",
-  },
-
-  colorScheme: "dark",
+  applicationName: domain,
+  keywords,
   themeColor: "#18181b",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-  },
-
-  icons: {
-    icon: [
-      {
-        url: "/assets/favicon-16x16.png",
-        type: "image/png",
-        sizes: "16x16",
-      },
-      {
-        url: "/assets/favicon-32x32.png",
-        type: "image/png",
-        sizes: "32x32",
-      },
-      {
-        url: "/assets/safari-pinned-tab.svg",
-        type: "image/svg+xml",
-        rel: "mask-icon",
-      },
-    ],
-    apple: {
-      url: "/apple-touch-icon.png",
-      sizes: "180x180",
-      type: "image/png",
-    },
-  },
+  colorScheme: "dark",
+  viewport: { width: "device-width", initialScale: 1 },
+  robots,
+  icons,
   manifest: "/site.webmanifest",
+  category: "Web Development",
 
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
+  openGraph,
+  twitter,
 
-  verification: {
-    google: process.env.VERIFICATION_GOOGLE,
-  },
+  verification: { google: process.env.VERIFICATION_GOOGLE },
+  assets: "/assets",
 };
 
 export default defaultMetadata;
